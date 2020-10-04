925a773d35fe2a8030b4e291b8a5b8f18bf5bf24
nuser <nuser@mbp.local>
2020-10-04 13:47:39 +0900

    init

---
+++ working_timer/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Timer/>
+  </div>
+</template>
+
+<script>
+import Timer from '@/components/Timer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Timer,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ working_timer/src/components/Timer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Timer',
+  props: {
+  },
+};
+</script>
+
+<style scoped>
+</style>
