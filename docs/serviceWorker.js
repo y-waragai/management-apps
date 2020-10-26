c7babe02c2c157232202d21721d43bb8681159db
nuser <nuser@mbp.local>
2020-10-26 09:20:04 +0900

    ビルド

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -7,4 +7,5 @@ self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
 
+// 現状では、この処理を書かないとService Workerが有効と判定されないようです
 self.addEventListener('fetch', function(event) {});
